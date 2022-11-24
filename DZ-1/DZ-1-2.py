5410db774b9eddcf69b3f81f7cf078d87f5cc3af
Alex Karnakov <ak2005@yandex.ru>
2022-11-24 16:05:23 +0300

    Fix bug in DZ-1-2

--- DZ-1/DZ-1-2.py
+++ DZ-1/DZ-1-2.py
@@ -5,7 +5,7 @@
 for x in range(0, 2):
     for y in range(0, 2):
         for z in range(0, 2):
-            if not (x or y or z) == (not x) and (not y) and (not z):
-                print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z}')
+            if (not (x or y or z)) == ((not x) and (not y) and (not z)):
+                print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} - TRUE')
             else:
-                print(f'¬({x} ⋁ {y} ⋁ {z}) != ¬{x} ⋀ ¬{y} ⋀ ¬{z}')
+                print(f'¬({x} ⋁ {y} ⋁ {z}) != ¬{x} ⋀ ¬{y} ⋀ ¬{z} - FALSE')
