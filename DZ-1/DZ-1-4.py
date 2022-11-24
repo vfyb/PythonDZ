9345ca7865a0b4e051baf82e7e3524bb15a4ebfb
Alex Karnakov <ak2005@yandex.ru>
2022-11-24 03:12:41 +0300

    DZ-1-4 added

--- DZ-1/DZ-1-4.py
+++ DZ-1/DZ-1-4.py
@@ -1,3 +1,15 @@
 # Напишите программу, которая по заданному номеру четверти, 
 # показывает диапазон возможных координат точек в этой четверти (x и y).
 
+user_number = int(input('Input number of quarter plane (1-4):\n'))
+
+if user_number < 1 or user_number > 4:
+    print('You entered wrong number.')
+elif user_number == 1:
+    print('Coordinate range: X > 0 and Y > 0')
+elif user_number == 2:
+    print('Coordinate range: X < 0 and Y > 0')
+elif user_number == 3:
+    print('Coordinate range: X < 0 and Y < 0')
+else:
+    print('Coordinate range: X > 0 and Y < 0')
