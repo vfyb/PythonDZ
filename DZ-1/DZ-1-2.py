e01f97b079d75a73a81692aef45918b98665ad33
Alex Karnakov <ak2005@yandex.ru>
2022-11-24 03:54:22 +0300

    DZ-1-2 added

--- DZ-1/DZ-1-2.py
+++ DZ-1/DZ-1-2.py
@@ -2,3 +2,10 @@
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 # (0,0,0), (0,0,1) и тд.
 
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            if not (x or y or z) == (not x) and (not y) and (not z):
+                print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z}')
+            else:
+                print(f'¬({x} ⋁ {y} ⋁ {z}) != ¬{x} ⋀ ¬{y} ⋀ ¬{z}')
