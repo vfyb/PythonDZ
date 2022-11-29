33c067b7a36363ad57109324fe1f695b463b1e78
Alex Karnakov <ak2005@yandex.ru>
2022-11-29 20:24:30 +0300

    DZ-1 added

--- DZ-2/DZ-1.py
+++ DZ-2/DZ-1.py
@@ -3,3 +3,15 @@
 # Пример:
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+user_number = int(input('Input number:\n'))
+factorial = 1
+
+if user_number < 0:
+    print(f'Cannot find factorial of {user_number}.')
+elif user_number == 0 or user_number == 1:
+    print(f'Factorial of number {user_number} = {factorial}')
+else:
+    for i in range(2, user_number + 1):
+        factorial = factorial * i
+    print(f'Factorial of number {user_number} = {factorial}')
+    
