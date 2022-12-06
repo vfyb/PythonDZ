3ee74ee64e23036b1edabf55dc0849cf2593f4c2
Alex Karnakov <ak2005@yandex.ru>
2022-12-06 16:49:32 +0300

    DZ-3 created

---
+++ DZ-3/DZ-1.py
@@ -0,0 +1,14 @@
+# Задайте список из нескольких чисел. Напишите программу, которая
+# найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Пример:
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+user_list = [4, 7, -3, 9, 6, -54, 67, 8]
+
+def GetSumOddElements(list):
+    sum_odd = 0
+    for i in range(1, len(list), 2):
+        sum_odd += list[i]
+    return sum_odd
+
+print(GetSumOddElements(user_list))
