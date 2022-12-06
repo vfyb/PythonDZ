104cd68ffca2d531d2ce82763250ee52bbbdf33c
Alex Karnakov <ak2005@yandex.ru>
2022-12-06 19:40:53 +0300

    DZ-2 added

---
+++ DZ-3/DZ-2.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+user_list1 = [2, 3, 4, 5, 6]
+user_list2 = [2, 3, 5, 6]
+
+
+def GetMultPairNumber(list):
+    result_list = []
+    
+    for i in range(round((len(list) + 1) / 2)):
+        result_list.append(list[i] * list[-i - 1])
+
+    return result_list
+
+
+print(GetMultPairNumber(user_list1))
+print(GetMultPairNumber(user_list2))
