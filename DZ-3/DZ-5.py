e61889a4ca213ba16310f95fb3944cb5a285d80a
Alex Karnakov <ak2005@yandex.ru>
2022-12-08 20:20:18 +0300

    DZ-4 added

--- DZ-3/DZ-5.py
+++ DZ-3/DZ-5.py
@@ -3,30 +3,49 @@
 # - для k = 8 список будет выглядеть так: 
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-user_number = int(input('Input number:\n'))
+# user_number = int(input('Input number:\n'))
 
-def GetFibonacci(number):
+# def GetFibonacci(number):
     
-    if number < 0:
-        print(f'Cannot create Fibonacci series for number {number}. Input number >= 0.')
-    elif number == 0:
-        print(f'Fibonacci series for {number} => 0')
-    elif number == 1:
-        print(f'Fibonacci series for {number} => [1, 0, 1]')
-    else:
-        fib_list_positive = [0, 1]
-        for i in range(2, number + 1):
-            fib_list_positive.append(fib_list_positive[i - 2] + fib_list_positive[i - 1])
+#     if number < 0:
+#         print(f'Cannot create Fibonacci series for number {number}. Input number >= 0.')
+#     elif number == 0:
+#         print(f'Fibonacci series for {number} => 0')
+#     elif number == 1:
+#         print(f'Fibonacci series for {number} => [1, 0, 1]')
+#     else:
+#         fib_list_positive = [0, 1]
+#         for i in range(2, number + 1):
+#             fib_list_positive.append(fib_list_positive[i - 2] + fib_list_positive[i - 1])
 
-    fib_list_negative = []
-    for i in range(1, len(fib_list_positive)):
-        if i % 2 == 0:
-            fib_list_negative.append(-fib_list_positive[i])
-        else:
-            fib_list_negative.append(fib_list_positive[i])
+#     fib_list_negative = []
+#     for i in range(1, len(fib_list_positive)):
+#         if i % 2 == 0:
+#             fib_list_negative.append(-fib_list_positive[i])
+#         else:
+#             fib_list_negative.append(fib_list_positive[i])
 
-    fib_list_negative.reverse()
+#     fib_list_negative.reverse()
 
-    return(fib_list_negative + fib_list_positive) 
+#     return(fib_list_negative + fib_list_positive) 
         
-print(f'Fibonacci series for {user_number} => {GetFibonacci(user_number)}')
+# print(f'Fibonacci series for {user_number} => {GetFibonacci(user_number)}')
+
+
+def get_fib(n):
+    fib_list = []
+    a, b = 1, 1
+    for i in range(n-1):
+        fib_list.append(a)
+        a, b = b, a + b
+
+    a, b = 0, 1
+    for i in range(n):
+        fib_list.insert(0, a)
+        a, b = b, a - b
+
+    return fib_list
+
+n = int(input()) + 1
+fib_list = get_fib(n)
+print(fib_list)
---
+++ DZ-4/DZ-1.py
@@ -0,0 +1,20 @@
+# Вычислить число Pi c заданной точностью d, не используя ф. round()
+# Пример:
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+import math
+
+PI = math.pi
+user_d = float(input('Input accuracy (for example 0.001):\n'))
+
+round_pi = int((PI / user_d) * 10)
+print(round_pi)
+
+if (round_pi % 10) > 4:
+    round_pi = int((round_pi + 10) / 10)
+else:
+    round_pi = int(round_pi / 10)
+
+print(round_pi)
+
+print(f'Rounded pi={PI} => {round_pi * user_d}')
